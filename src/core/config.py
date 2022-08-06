6c15aa92c1548a6de85f98ecaca4e97141502a46
Ivan <grecigor11@gmail.com>
2022-08-06 17:36:25 +0300

    deleted default params

--- src/core/config.py
+++ src/core/config.py
@@ -4,14 +4,13 @@ import os
 class Settings:
     DB = os.getenv(
         "DATABASE_URL",
-        default="postgresql+asyncpg://test1:test3004@localhost/test_fastapi",
     )
     TEST_DB = os.getenv(
-        "TEST_DATABASE", default="postgresql+asyncpg://test1:test3004@localhost/test"
+        "TEST_DATABASE"
     )
-    SECRET = os.getenv("SECRET_KEY", default="SECRET")
+    SECRET = os.getenv("SECRET_KEY")
     DSN = os.getenv("DSN")
-    REDIS_HOST = os.getenv("REDIS_HOST", default="redis://localhost:6379/0")
+    REDIS_HOST = os.getenv("REDIS_HOST")
 
 
 settings = Settings()
