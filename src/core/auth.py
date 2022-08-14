e783a919fe9cac8ebfda9cd5cbfc0d728bbfc437
Ivan <grecigor11@gmail.com>
2022-08-14 18:04:22 +0300

    changed error handler

--- src/core/auth.py
+++ src/core/auth.py
@@ -32,9 +32,9 @@ class AuthHandler:
             payload = jwt.decode(token, self.secret, algorithms=["HS256"])
             return payload
         except jwt.ExpiredSignatureError:
-            return HTTPException(status_code=401, detail="Signature has expired")
+            raise HTTPException(status_code=401, detail="Signature has expired")
         except jwt.InvalidTokenError:
-            return HTTPException(status_code=401, detail="Token is invalid")
+            raise HTTPException(status_code=401, detail="Token is invalid")
 
     def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
         return self.decode_token(auth.credentials)
